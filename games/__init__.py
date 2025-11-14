0307df56606202374bb2df59b8cd86c83ae1f48b
عبير <alqaws151@gmail.com>
2025-11-14 17:47:16 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -7,7 +7,7 @@
 from .iq_game import IQGame
 from .word_color_game import WordColorGame
 from .chain_words_game import ChainWordsGame
-from .scramble_word_game import ScrambleWordGame
+from .scramble_word_game import ScrambleWordGameAdvanced
 from .letters_words_game import LettersWordsGame
 from .fast_typing_game import FastTypingGame
 from .human_animal_plant_game import HumanAnimalPlantGame
@@ -24,7 +24,7 @@ __all__ = [
     'IQGame',
     'WordColorGame',
     'ChainWordsGame',
-    'ScrambleWordGame',
+    'ScrambleWordGameAdvanced',  # النسخة الجديدة
     'LettersWordsGame',
     'FastTypingGame',
     'HumanAnimalPlantGame',
@@ -36,61 +36,4 @@ __all__ = [
     'OppositeGame',
     'EmojiGame',
     'SongGame'
-]
-
-
-# utils/__init__.py
-"""
-أدوات مساعدة للبوت
-"""
-
-from .helpers import (
-    get_user_profile_safe,
-    normalize_text,
-    check_rate_limit,
-    cleanup_old_games
-)
-
-from .database import (
-    init_db,
-    update_user_points,
-    get_user_stats,
-    get_leaderboard
-)
-
-from .ui_components import (
-    get_games_quick_reply,
-    get_winner_announcement,
-    get_help_message,
-    get_welcome_message,
-    get_stats_message,
-    get_leaderboard_message,
-    get_join_message
-)
-
-from .gemini_config import (
-    USE_AI,
-    get_gemini_api_key,
-    switch_gemini_key
-)
-
-__all__ = [
-    'get_user_profile_safe',
-    'normalize_text',
-    'check_rate_limit',
-    'cleanup_old_games',
-    'init_db',
-    'update_user_points',
-    'get_user_stats',
-    'get_leaderboard',
-    'get_games_quick_reply',
-    'get_winner_announcement',
-    'get_help_message',
-    'get_welcome_message',
-    'get_stats_message',
-    'get_leaderboard_message',
-    'get_join_message',
-    'USE_AI',
-    'get_gemini_api_key',
-    'switch_gemini_key'
 ]
