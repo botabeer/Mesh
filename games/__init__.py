4cbab4b152ac52f19c3d0c35c4f1b6dbdb976adf
عبير <alqaws151@gmail.com>
2025-11-21 19:57:22 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,22 +1,11 @@
-class GameName:
-    def __init__(self, line_bot_api=None, use_ai=False, get_api_key=None, switch_key=None):
-        """
-        تهيئة اللعبة بشكل موحد لكل الألعاب.
+from .word_formation import WordFormationGame
+from .fast_typing import FastTypingGame
+from .math_game import MathGame
+from .scramble import ScrambleGame
 
-        Args:
-            line_bot_api: كائن LineBotApi للتواصل مع LINE.
-            use_ai (bool): هل تستخدم الذكاء الاصطناعي في اللعبة.
-            get_api_key (function): دالة لإرجاع مفتاح API للذكاء الاصطناعي.
-            switch_key (function): دالة لتغيير مفتاح AI في حال فشل المفتاح الحالي.
-        """
-        self.line_bot_api = line_bot_api
-        self.use_ai = use_ai
-        self.get_api_key = get_api_key
-        self.switch_key = switch_key
-
-        # متغيرات خاصة باللعبة
-        self.current_question = None
-        self.current_answer = None
-        self.hint_used = False
-
-        # أي إعدادات أخرى خاصة باللعبة يمكن إضافتها هنا
+__all__ = [
+    'WordFormationGame',
+    'FastTypingGame',
+    'MathGame',
+    'ScrambleGame'
+]
