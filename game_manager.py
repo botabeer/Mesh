88aa0241f6896e4b93593d505348eef88e54e727
عبير <alqaws151@gmail.com>
2025-11-23 19:03:46 +0300

    Update game_manager.py

--- game_manager.py
+++ game_manager.py
@@ -3,42 +3,41 @@ Bot Mesh - Game Manager
 """
 class GameManager:
     def __init__(self):
-        self.active_games = {}   # gid -> {'game': GameObject, 'type': 'ذكاء'}
-        self.user_answers = {}   # gid -> set(uid)
+        self.active_games = {}  # gid: {"game": game_obj, "type": name}
+        self.registered_users = set()
+        self.answered_users = {}
 
     def register(self, uid):
-        # placeholder: maybe track users for statistics
-        pass
+        self.registered_users.add(uid)
 
-    def remove_user(self, uid):
-        for gid in list(self.active_games.keys()):
-            self.user_answers.get(gid,set()).discard(uid)
+    def unregister(self, uid):
+        self.registered_users.discard(uid)
+        for gid in self.answered_users:
+            self.answered_users[gid].discard(uid)
+
+    def is_registered(self, uid):
+        return uid in self.registered_users
+
+    def start_game(self, gid, game, game_type):
+        self.active_games[gid] = {"game": game, "type": game_type}
+        self.answered_users[gid] = set()
 
     def get_game(self, gid):
         return self.active_games.get(gid)
 
-    def start_game(self, gid, game, game_type):
-        self.active_games[gid] = {'game': game, 'type': game_type}
-        self.user_answers[gid] = set()
-
     def end_game(self, gid):
         self.active_games.pop(gid, None)
-        self.user_answers.pop(gid, None)
+        self.answered_users.pop(gid, None)
 
     def has_answered(self, gid, uid):
-        return uid in self.user_answers.get(gid,set())
+        return uid in self.answered_users.get(gid, set())
 
     def mark_answered(self, gid, uid):
-        if gid not in self.user_answers:
-            self.user_answers[gid] = set()
-        self.user_answers[gid].add(uid)
-
-    def is_registered(self, uid):
-        # placeholder: we only allow registered users to play
-        return True
+        if gid in self.answered_users:
+            self.answered_users[gid].add(uid)
 
     def get_users_count(self):
-        return sum(len(u) for u in self.user_answers.values())
+        return len(self.registered_users)
 
     def get_active_games_count(self):
         return len(self.active_games)
