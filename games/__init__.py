0c60b2ed1145e3516423f544d65a84eb89d45110
عبير <alqaws151@gmail.com>
2025-11-26 01:35:20 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,15 +1,8 @@
 """
-Bot Mesh - Games Package
-Created by: Abeer Aldosari © 2025
-
-This package contains all game modules for Bot Mesh.
-Each game inherits from BaseGame and implements:
-- next_question(): Generate next question
-- check_answer(): Validate user answer
+Bot Mesh v6.1 - Games Package
 """
 
-from games.base_game import BaseGame
+from .game_loader import load_games
 
-__all__ = ['BaseGame']
-__version__ = '3.0.0'
-__author__ = 'Abeer Aldosari'
+__all__ = ['load_games']
+__version__ = '6.1.0'
