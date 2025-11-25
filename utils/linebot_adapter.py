921ce034c0697509c7239fa738ee060322313dc5
عبير <alqaws151@gmail.com>
2025-11-26 01:21:11 +0300

    Delete utils directory

--- utils/linebot_adapter.py
+++
@@ -1,9 +0,0 @@
-# Improved adapter
-class LineBotAPI:
-    def __init__(self,*a,**k): pass
-    def reply_message(self,reply_token,msgs): print("Stub reply:",msgs)
-
-class WebhookHandler:
-    def __init__(self,*a,**k): pass
-    def handle(self,*a,**k): pass
-    def add(self,*a,**k): pass
