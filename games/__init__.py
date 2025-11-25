ce9b4d9a8eebee9a9719c06423e3901f14192731
عبير <alqaws151@gmail.com>
2025-11-25 11:00:13 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,32 +1,15 @@
 """
 Bot Mesh - Games Package
 Created by: Abeer Aldosari © 2025
+
+This package contains all game modules for Bot Mesh.
+Each game inherits from BaseGame and implements:
+- next_question(): Generate next question
+- check_answer(): Validate user answer
 """
 
-from .iq_game import IqGame
-from .math_game import MathGame
-from .word_color_game import WordColorGame
-from .scramble_word_game import ScrambleWordGame
-from .fast_typing_game import FastTypingGame
-from .opposite_game import OppositeGame
-from .letters_words_game import LettersWordsGame
-from .song_game import SongGame
-from .human_animal_plant_game import HumanAnimalPlantGame
-from .chain_words_game import ChainWordsGame
-from .guess_game import GuessGame
-from .compatibility_game import CompatibilityGame
+from games.base_game import BaseGame
 
-__all__ = [
-    'IqGame',
-    'MathGame',
-    'WordColorGame',
-    'ScrambleWordGame',
-    'FastTypingGame',
-    'OppositeGame',
-    'LettersWordsGame',
-    'SongGame',
-    'HumanAnimalPlantGame',
-    'ChainWordsGame',
-    'GuessGame',
-    'CompatibilityGame'
-]
+__all__ = ['BaseGame']
+__version__ = '3.0.0'
+__author__ = 'Abeer Aldosari'
