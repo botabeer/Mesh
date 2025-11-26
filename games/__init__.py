81e45fe5584c9c55754f265d3bf5670dcb736d12
عبير <alqaws151@gmail.com>
2025-11-26 17:09:42 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,8 +1,20 @@
 """
-Bot Mesh v6.1 - Games Package
+🎮 Bot Mesh v7.0 - Games Package
+حزمة الألعاب المتكاملة
+Created by: Abeer Aldosari © 2025
 """
 
-from .game_loader import load_games
+from .base_game import BaseGame
 
-__all__ = ['load_games']
-__version__ = '6.1.0'
+__all__ = ['BaseGame']
+__version__ = '7.0.0'
+__author__ = 'Abeer Aldosari'
+
+# معلومات الحزمة
+PACKAGE_INFO = {
+    'name': 'Bot Mesh Games',
+    'version': __version__,
+    'author': __author__,
+    'description': 'مجموعة ألعاب ترفيهية لبوت LINE',
+    'year': 2025
+}
