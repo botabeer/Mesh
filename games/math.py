64e78ca4440c47e9afa9aa757c98a6c1127bbfb1
عبير <alqaws151@gmail.com>
2025-12-18 22:55:32 +0300

    Update math.py

--- games/math.py
+++ games/math.py
@@ -3,8 +3,6 @@ from games.base import BaseGame
 
 
 class MathGame(BaseGame):
-    """لعبة رياضيات محسّنة"""
-    
     def __init__(self, db, theme: str = "light"):
         super().__init__(db, theme)
         self.game_name = "رياضيات"
